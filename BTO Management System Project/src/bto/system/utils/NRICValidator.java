ebc3469970aa31ef5eca894bb6f371efa6d988d0
limk0 <LIMK0160@e.ntu.edu.sg>
2025-04-14 21:36:45 +0800

    Update NRICValidator.java

--- BTO Management System Project/src/bto/system/utils/NRICValidator.java
+++ BTO Management System Project/src/bto/system/utils/NRICValidator.java
@@ -3,7 +3,7 @@ package bto.system.utils;
 public class NRICValidator {
     public static boolean isValidNRIC(String nric) {
         return nric != null &&
-                nric.matches("[STFG]\\d{7}[A-Z]") &&
+                nric.matches("[ST]\\d{7}[A-Z]") &&
                 nric.length() == 9;
     }
 }
