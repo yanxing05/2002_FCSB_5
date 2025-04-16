5750d4ebb582f6f82662dff528ef599d9074b029
yanxing05 <euyanxing@gmail.com>
2025-04-16 10:20:00 +0800

    Update ApplicationController.java

--- BTO Management System Project/src/bto/system/controllers/ApplicationController.java
+++ BTO Management System Project/src/bto/system/controllers/ApplicationController.java
@@ -2,8 +2,11 @@ package bto.system.controllers;
 
 import bto.system.models.Application;
 import bto.system.models.BTOProject;
+import bto.system.models.Enquiry;
 import bto.system.models.users.Applicant;
 import bto.system.services.ApplicationService;
+
+import java.util.List;
 
 public class ApplicationController {
     private final ApplicationService applicationService;
@@ -16,7 +19,31 @@ public class ApplicationController {
         return applicationService.createApplication(applicant, project);
     }
 
-    public void withdrawApplication(Application application) {
-        applicationService.withdrawApplication(application);
+    public void withdrawApplication(Applicant applicant) {
+        applicationService.withdrawApplication(applicant);
     }
+
+    public Application getApplication(Applicant applicant) {
+        return applicationService.getApplication(applicant);
+    }
+
+    public boolean hasActiveApplication(Applicant applicant) {
+        return applicationService.hasActiveApplication(applicant);
+    }
+
+    public void addEnquiry(Applicant applicant, Enquiry enquiry) {
+        applicationService.addEnquiry(applicant, enquiry);
+    }
+
+    public void deleteEnquiry(Applicant applicant, Enquiry enquiry) {
+        applicationService.deleteEnquiry(applicant, enquiry);
+    }
+
+    public List<Enquiry> getEnquiries(Applicant applicant) {
+        return applicationService.getEnquiries(applicant);
+    }
+
+    public List<Application> getAllApplications() {
+        return applicationService.getAllApplications();
+    }    
 }
