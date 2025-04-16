8612e3facb220f892726cae99ac44fd8425d4efe
yanxing05 <euyanxing@gmail.com>
2025-04-16 10:22:25 +0800

    Update ApplicationService.java

--- BTO Management System Project/src/bto/system/services/ApplicationService.java
+++ BTO Management System Project/src/bto/system/services/ApplicationService.java
@@ -2,12 +2,14 @@ package bto.system.services;
 
 import bto.system.models.Application;
 import bto.system.models.BTOProject;
+import bto.system.models.Enquiry;
 import bto.system.models.users.Applicant;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class ApplicationService {
-    private List<Application> applications;
+    private final List<Application> applications;
 
     public ApplicationService() {
         this.applications = new ArrayList<>();
@@ -16,10 +18,47 @@ public class ApplicationService {
     public Application createApplication(Applicant applicant, BTOProject project) {
         Application application = new Application(applicant, project);
         applications.add(application);
+        applicant.setApplication(application);
         return application;
     }
 
-    public void withdrawApplication(Application application) {
-        application.setStatus("Withdrawn");
+    public void withdrawApplication(Applicant applicant) {
+        Application app = applicant.getApplication();
+        if (app != null) {
+            app.setStatus("Withdrawn");
+            applicant.withdrawApplication(); // sets to null
+        }
+    }
+
+    public Application getApplication(Applicant applicant) {
+        return applicant.getApplication();
+    }
+
+    public boolean hasActiveApplication(Applicant applicant) {
+        Application app = applicant.getApplication();
+        return app != null && app.isActive();
+    }
+
+    public void addEnquiry(Applicant applicant, Enquiry enquiry) {
+        Application app = applicant.getApplication();
+        if (app != null) {
+            app.addEnquiry(enquiry);
+        }
+    }
+
+    public void deleteEnquiry(Applicant applicant, Enquiry enquiry) {
+        Application app = applicant.getApplication();
+        if (app != null) {
+            app.getEnquiries().remove(enquiry); // you could improve this with a custom method
+        }
+    }
+
+    public List<Enquiry> getEnquiries(Applicant applicant) {
+        Application app = applicant.getApplication();
+        return app != null ? app.getEnquiries() : new ArrayList<>();
+    }
+
+    public List<Application> getAllApplications() {
+        return applications;
     }
 }
