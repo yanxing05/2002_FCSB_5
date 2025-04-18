822313f29d5521d98d9898333f1b98f1bf86685e
yanxing05 <euyanxing@gmail.com>
2025-04-19 00:37:11 +0800

    Update ApplicationController.java

--- BTO Management System Project/src/bto/system/controllers/ApplicationController.java
+++ BTO Management System Project/src/bto/system/controllers/ApplicationController.java
@@ -2,7 +2,6 @@ package bto.system.controllers;
 
 import bto.system.models.Application;
 import bto.system.models.BTOProject;
-import bto.system.models.Enquiry;
 import bto.system.models.users.Applicant;
 import bto.system.services.ApplicationService;
 
@@ -31,17 +30,6 @@ public class ApplicationController {
         return applicationService.hasActiveApplication(applicant);
     }
 
-    public void addEnquiry(Applicant applicant, Enquiry enquiry) {
-        applicationService.addEnquiry(applicant, enquiry);
-    }
-
-    public void deleteEnquiry(Applicant applicant, Enquiry enquiry) {
-        applicationService.deleteEnquiry(applicant, enquiry);
-    }
-
-    public List<Enquiry> getEnquiries(Applicant applicant) {
-        return applicationService.getEnquiries(applicant);
-    }
 
     public List<Application> getAllApplications() {
         return applicationService.getAllApplications();
